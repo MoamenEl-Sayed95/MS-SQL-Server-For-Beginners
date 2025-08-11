bece4193bce321c4335f7b82ec1af0613e03acc1
MoamenEl-Sayed95 <moamenprogrammer95@gmail.com>
2025-08-11 15:20:03 +0300

    Solve SQL challenge Weather-Observation-Station-1

---
+++ Practice-SQL-Queries/Weather-Observation-Station-1.sql
@@ -0,0 +1,10 @@
+/*
+Query a list of CITY and STATE from the STATION table.
+The STATION table is described as follows:
+*/
+
+https://s3.amazonaws.com/hr-challenge-images/9336/1449345840-5f0a551030-Station.jpg
+
+-- HackerRank -> Solution:
+
+SELECT city, state FROM station;
