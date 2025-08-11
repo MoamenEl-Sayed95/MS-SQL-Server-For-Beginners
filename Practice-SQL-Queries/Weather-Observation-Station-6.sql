bb3ce8ab876415b57e2d2ec988822bae2ad546b1
MoamenEl-Sayed95 <moamenprogrammer95@gmail.com>
2025-08-11 17:05:00 +0300

    Solve SQL challenge Weather-Observation-Station-6

---
+++ Practice-SQL-Queries/Weather-Observation-Station-6.sql
@@ -0,0 +1,14 @@
+/*
+Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION.
+Your result cannot contain duplicates.
+Input Format
+The STATION table is described as follows:
+*/
+
+https://s3.amazonaws.com/hr-challenge-images/9336/1449345840-5f0a551030-Station.jpg
+
+-- HackerRank -> Solution:
+
+SELECT DISTINCT city 
+FROM station 
+WHERE LOWER(SUBSTRING(city,1,1)) IN ('e','a','i','o','u');
