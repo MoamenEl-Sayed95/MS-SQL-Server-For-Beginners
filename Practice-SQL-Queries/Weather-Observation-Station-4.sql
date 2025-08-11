96edc3bbf1aac98ff0adf566669ad322487d1340
MoamenEl-Sayed95 <moamenprogrammer95@gmail.com>
2025-08-11 16:51:55 +0300

    Solve SQL challenge Weather-Observation-Station-4

---
+++ Practice-SQL-Queries/Weather-Observation-Station-4.sql
@@ -0,0 +1,11 @@
+/*
+Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
+The STATION table is described as follows:
+*/
+
+https://s3.amazonaws.com/hr-challenge-images/9336/1449345840-5f0a551030-Station.jpg
+
+-- HackerRank -> Solution:
+
+SELECT COUNT(*) - COUNT(DISTINCT city)
+FROM station;
